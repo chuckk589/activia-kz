f5379b4ad1588031922cd2f5d7fb863ccedaba83
chuckk589 <crazy-rabit@yandex.ru>
2022-05-13 05:40:42 +0300

    env upd

--- src/app.module.ts
+++ src/app.module.ts
@@ -34,13 +34,13 @@ import { MySqlDriver } from '@mikro-orm/mysql/MySqlDriver';
         return {
           type: 'mysql',
           allowGlobalContext: true,
-          ...(configService.get('node_env') === 'production'
-            ? {
+          ...(configService.get('node_env') === 'development'
+            ? { debug: true, logger: console.log.bind(console) }
+            : {
                 driverOptions: {
                   connection: { socketPath: '/var/lib/mysql/mysql.sock' },
                 },
-              }
-            : { debug: true, logger: console.log.bind(console) }),
+              }),
           entities: ['./dist/modules/mikroorm/entities/'],
           entitiesTs: ['./src/modules/mikroorm/entities/'],
           clientUrl: configService.get('database', { infer: true }),
